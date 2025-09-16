16475824d7c3fba61245cc3eead42e054a82df66
JoeyJia <1652391561@qq.com>
2025-09-16 16:20:19 +0800

    9.16/16:20

--- BMI/app/src/main/java/com/example/bmi/MainActivity.kt
+++ BMI/app/src/main/java/com/example/bmi/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.bmi
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,36 +15,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bmi.ui.theme.BMITheme
+import android.widget.Button
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    lateinit var heightET: EditText
+    lateinit var weightET: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            BMITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        heightET=findViewById(R.id.heightET)
+        weightET=findViewById(R.id.weightET)
+
+        val reportBtn: Button =findViewById(R.id.reportBtn)
+        reportBtn.setOnKeyListener(object :View.OnclickListener) {
+            override fun onClick(v:view?){
+                val height=heightET.text.toString()
+                val weight=weightET.text.toString()
+                val intent = Intent(applicationContext,ReportActivity::class.java)
+                val vundle = Bundle()
+                bundle.putString("weight",weight)
+                bundle.putString("height",height)
+                intent.putExtras(bundle)
+                startActivity(intent)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BMITheme {
-        Greeting("Android")
     }
 }
